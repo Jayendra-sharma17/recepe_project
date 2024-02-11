14a29abf0a06f9fa5de847de0b1a69ec345b9843
jayendra <2020pietcsjayendra85@poornima.org>
2024-02-11 19:39:24 +0530

    recepies project 1

---
+++ vege/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Receipe(models.Model):
+    Receipe_name=models.CharField(max_length=100)
+    Receipe_description=models.TextField()
+    Receipe_image=models.ImageField(upload_to="receipe")
