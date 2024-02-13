4fd986529049fc4f436b1b3a7428557996f6430c
jayendra <2020pietcsjayendra85@poornima.org>
2024-02-13 10:36:42 +0530

    updated modelpy

--- vege/models.py
+++ vege/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User #yee django ka pre-built model hota h 
+
+
 
 # Create your models here.
 
 class Receipe(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     receipe_name=models.CharField(max_length=100)
     receipe_description=models.TextField()
     receipe_image=models.ImageField(upload_to="receipe")
